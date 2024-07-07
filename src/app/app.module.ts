9daaaf12b7f6219b43107ba87b539d71cd4533aa
Riya Amemiya <riyaamemiya@gmail.com>
2024-07-08 02:25:51 +0900

    feat: NgPipesModuleを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgPipesModule } from "ngx-pipes";
 import { ConfirmationService } from "primeng/api";
 
 import { AppComponent } from "./app.component";
-
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppComponent,
+    NgPipesModule,
+  ],
   providers: [ConfirmationService],
 })
 export class AppModule {}
