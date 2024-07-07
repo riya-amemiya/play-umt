7bd36db50fcd0e6d99df6d92e44f944917b1a552
Riya Amemiya <riyaamemiya@gmail.com>
2024-07-08 02:05:54 +0900

    feat: NgRxをプロジェクトに追加

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,24 @@
 import {
   type ApplicationConfig,
-  provideZoneChangeDetection,
+  provideZoneChangeDetection, isDevMode,
 } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { provideClientHydration } from "@angular/platform-browser";
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideRouterStore } from '@ngrx/router-store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-  ],
+    provideStore(),
+    provideEffects(),
+    provideRouterStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
+],
 };
