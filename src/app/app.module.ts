e0ea3d8e7a27bf6db043de45698fea87a65edf2d
Riya Amemiya <riyaamemiya@gmail.com>
2024-07-11 01:02:51 +0900

    feat: Aboutモジュールを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,16 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ConfirmationService } from "primeng/api";
 
 import { AppComponent } from "./app.component";
+import { AboutModule } from "./routes/about/about.module";
 import { HomeModule } from "./routes/home/home.module";
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, AppComponent, HomeModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppComponent,
+    HomeModule,
+    AboutModule,
+  ],
   providers: [ConfirmationService],
 })
 export class AppModule {}
