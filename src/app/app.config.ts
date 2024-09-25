a241117f37c190df793fbb9aae78959d42023c1b
Yuta Nishi <riyaamemiya@gmail.com>
2024-09-25 23:31:15 +0900

    refactor: Firebase関連コードと依存関係を削除

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,4 @@
 import { type ApplicationConfig, isDevMode } from "@angular/core";
-import {
-  getAnalytics,
-  provideAnalytics,
-  ScreenTrackingService,
-  UserTrackingService,
-} from "@angular/fire/analytics";
-import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
-import { getAuth, provideAuth } from "@angular/fire/auth";
-import { getFirestore, provideFirestore } from "@angular/fire/firestore";
-import { getFunctions, provideFunctions } from "@angular/fire/functions";
-import { getPerformance, providePerformance } from "@angular/fire/performance";
-import {
-  getRemoteConfig,
-  provideRemoteConfig,
-} from "@angular/fire/remote-config";
-import { getStorage, provideStorage } from "@angular/fire/storage";
 import { provideClientHydration } from "@angular/platform-browser";
 import { provideRouter } from "@angular/router";
 import { provideEffects } from "@ngrx/effects";
@@ -34,35 +18,5 @@ export const appConfig: ApplicationConfig = {
     provideEffects(),
     provideRouterStore(),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: import.meta.env.FIREBASE_PROJECT_ID,
-        appId: import.meta.env.FIREBASE_APP_ID,
-        storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-        apiKey: import.meta.env.FIREBASE_API_KEY,
-        authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-        messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-        measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
-      }),
-    ),
-    provideAuth(() => getAuth()),
-    provideAnalytics(() => getAnalytics()),
-    ScreenTrackingService,
-    UserTrackingService,
-    // provideAppCheck(() => {
-    //   // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
-    //   const provider = new ReCaptchaEnterpriseProvider(
-    //     /* reCAPTCHA Enterprise site key */
-    //   );
-    //   return initializeAppCheck(undefined, {
-    //     provider,
-    //     isTokenAutoRefreshEnabled: true,
-    //   });
-    // }),
-    provideFirestore(() => getFirestore()),
-    provideFunctions(() => getFunctions()),
-    providePerformance(() => getPerformance()),
-    provideStorage(() => getStorage()),
-    provideRemoteConfig(() => getRemoteConfig()),
   ],
 };
